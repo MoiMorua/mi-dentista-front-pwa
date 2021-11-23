46e572ce0ccee618289e048587a62583d6f1efa6
MoiMorua <57070474+MoiMorua@users.noreply.github.com>
2021-11-22 18:38:11 -0600

    Se modifica pagina de citas

--- src/pages/AppointmentPage/AppointmentPage.jsx
+++ src/pages/AppointmentPage/AppointmentPage.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const AppointmentPage = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <div className="AppointmentPage">
+                <header className="Service-header">
+                    <h1>Citas registradas</h1>
+                </header> 
+            </div>
+        </>
     )
 }
 
